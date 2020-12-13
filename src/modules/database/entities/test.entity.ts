3c23de8c3a74de2d138644c83bcac15d8a956f97
seonggukchoi <seonggukchoi.kr@gmail.com>
2020-12-13 19:19:02 +0900

    Add data model structure

--- src/modules/database/entities/test.entity.ts
+++ src/modules/database/entities/test.entity.ts
@@ -1,9 +1,11 @@
 import { Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+import { ITestModel } from '@app/models/data-models';
+
 import { BaseEntity } from './base.entity';
 
 @Entity({ name: 'test' })
-export class TestEntity extends BaseEntity {
+export class TestEntity extends BaseEntity implements ITestModel {
   @PrimaryGeneratedColumn()
   public id!: number;
 }

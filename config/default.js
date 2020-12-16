bbcbc02bd79fa43534cc141ad329bc460b095148
seonggukchoi <seonggukchoi.kr@gmail.com>
2020-12-16 16:54:01 +0900

    Separate dotenv loader

--- config/default.js
+++ config/default.js
@@ -1,17 +1,7 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
 
-const dotenv = require('dotenv');
-const path = require('path');
-
-const defaultDotenvPath = path.join(__dirname, '../');
-
-[
-  path.join(defaultDotenvPath, '.env.local'),
-  path.join(defaultDotenvPath, `.env.${process.env.NODE_ENV}.local`),
-  path.join(defaultDotenvPath, `.env.${process.env.NODE_ENV}`),
-  path.join(defaultDotenvPath, '.env'),
-].forEach((path) => dotenv.config({ path }));
+require('./dotenv-loader');
 
 module.exports = {
   application: {
